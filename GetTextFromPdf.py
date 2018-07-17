370231a783f28ae27bb041d91560248601dbf260
DmrfCoder <xuefanggag97@gmail.com>
2018-07-18 00:33:09 +0800

    init

---
+++ GetTextFromPdf.py
@@ -0,0 +1,41 @@
+#  -*- encoding: utf-8 -*-
+from wand.image import Image
+from PIL import Image as PI
+import pyocr
+import pyocr.builders
+import io
+import pytesseract
+
+
+def getTxtFromPdf(PdfPath):
+    #  pyocr支持两种OCR库，由于我只安装了tesseract，只会获得tesseract
+    tool = pyocr.get_available_tools()[0]
+    #  选择要使用的语言，使用print tool.get_available_languages()列表
+    #print '支持的语言:' + tool.get_available_languages()
+
+
+    #lang = tool.get_available_languages()[2]
+    #  用来保存图像和对应的文字
+    req_image = []
+    final_text = []
+    #  打开pdf文件，并转为图像，替换./test.pdf
+    image_pdf = Image(filename=PdfPath, resolution=300)
+    image_jpeg = image_pdf.convert('jpeg')
+    image_jpeg.save(filename='demo.jpeg')
+
+
+    #  把图片放到req_image中
+    for img in image_jpeg.sequence:
+        img_page = Image(image=img)
+        req_image.append(img_page.make_blob('jpeg'))
+    #  为每个图像运行OCR，识别图像中的文本
+        for img in req_image:
+            code = pytesseract.image_to_string(PI.open(io.BytesIO(img)), lang='chi_sim')
+            print(code)
+
+
+
+if __name__ == "__main__":
+    PdfPath = '/home/dmrfcoder/Document/CSI/IOT.pdf'
+    getTxtFromPdf(PdfPath)
+    #print 'final txt:'+Text
---
+++ PdfToPic.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import io
+
+from wand.image import Image
+from wand.color import Color
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+memo = {}
+
+def getPdfReader(filename):
+    reader = memo.get(filename, None)
+    if reader is None:
+        reader = PdfFileReader(filename, strict=False)
+        memo[filename] = reader
+    return reader
+
+def _run_convert(filename, page, res=120):
+    idx = page + 1
+    pdfile = getPdfReader(filename)
+    pageObj = pdfile.getPage(page)
+    dst_pdf = PdfFileWriter()
+    dst_pdf.addPage(pageObj)
+
+    pdf_bytes = io.BytesIO()
+    dst_pdf.write(pdf_bytes)
+    pdf_bytes.seek(0)
+
+    img = Image(file=pdf_bytes, resolution=res)
+    img.format = 'png'
+    img.compression_quality = 90
+    img.background_color = Color("white")
+    img_path = '%s%d.png' % (filename[:filename.rindex('.')], idx)
+    img.save(filename=img_path)
+    img.destroy()
+
+if __name__ == "__main__":
+    PdfPath = '/home/dmrfcoder/Document/CSI/IOT.pdf'
+    _run_convert(PdfPath,3)
